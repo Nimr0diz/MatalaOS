274a60f6f20fa48ceb8c6c919ea43c77240cd8bc
izraelyni <nimrodizr@gmail.com>
2018-01-17 12:15:13 +0200

    Updating "New" Class #2
    Adding "New" cpp file
    Adding static methods for genre

--- New.cpp
+++ New.cpp
@@ -2,3 +2,77 @@
 // Created by izraelyni on 17/01/2018.
 //
 
+#include "New.h"
+
+    New::New(const string& content)
+    {
+        stringstream s(content);
+        string dump;
+        string genreString;
+        s >> dump >> producerId >> genreString >> serialId;
+        genre = genreToString(genreString);
+
+    }
+
+    New::New(int producerId, int serialId, Genre genre)
+    {
+        this->producerId = producerId;
+        this->serialId = serialId;
+        this->genre = genre;
+    }
+
+    int New::getProducerId()
+    {
+        return producerId;
+    }
+
+    void New::setProducerId(int producerId)
+    {
+        this->producerId = producerId;
+    }
+
+    int New::getSerialId()
+    {
+        return serialId;
+    }
+
+    void New::setSerialId(int serialId)
+    {
+        this->serialId = serialId;
+    }
+
+    Genre New::getGenre()
+    {
+        return genre;
+    }
+
+    void New::setGenre(Genre genre)
+    {
+        this->genre = genre;
+    }
+
+    string New::getContent()
+    {
+        return "producer"+" "+producerId+" "+genreToString(genre)+" "+serialId;
+    }
+
+    static string New::genreToString(Genre gen)
+    {
+        if(gen == Genre::NEWS)
+            return "NEWS";
+        if(gen == Genre::SPORT)
+            return "SPORT";
+        if(gen == Genre::WEATHER)
+            return "WEATHER";
+        else return "";
+    }
+
+    static Genre New::stringToGenre(string str)
+    {
+        if(genreString.compare("NEWS"))
+            return Genre::NEWS;
+        if(genreString.compare("SPORT"))
+            return Genre::SPORT;
+        if(genreString.compare("WEATHER"))
+            return Genre::WEATHER;
+    }
--- New.h
+++ New.h
@@ -5,11 +5,18 @@
 #ifndef MATALAOS_NEW_H
 #define MATALAOS_NEW_H
 
+#include <iostream>
+#include <sstream>
+
 enum Genre{NEWS,SPORT,WEATHER};
 
 using namespace std;
 
 class New{
+protected:
+    int producerId;
+    int serialId;
+    Genre genre;
 public:
     New(const string& content);
     New(int producerId, int serialId, Genre genre);
@@ -24,6 +31,9 @@ public:
     void setGenre(Genre genre);
 
     string getContent();
+
+    static string genreToString(Genre gen);
+    static Genre stringToGenre(string str);
 };
 
 
